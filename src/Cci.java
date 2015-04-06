f618a9d81f6069b44d7d69bcb2696144cc339880
martin <martin@debian.polytope.org>
2015-04-06 21:46:38 +0100

    Updates for cci book probs

--- src/Cci.java
+++ src/Cci.java
@@ -58,4 +58,127 @@ class Cci {
 		
 	}
 	
+	static public void spaceReplace(char[] str, int len) {
+
+		//increment this by 2 as we insert '%20'
+		int newlen = len;
+
+		for(int i = 0; i<newlen; i++) {
+			if(str[i] == ' ')  {
+				newlen+=2;
+				//shunt the remainder of the string along by 2
+				for(int j = newlen-1; j>i;j--) {
+					str[j] = str[j-2];
+				}
+				str[i]='%';
+				str[i+1]='2';
+				str[i+2]='0';
+				i+=2;
+			}
+		}
+	
+	}
+	
+	static public String compressString(String in) {
+		//we are given assumption that string only contains upper or lower case a-z
+		//ignoring that assumption for now:
+
+		if(in.length() <=1 )
+		{
+			return in;
+		}
+
+		StringBuffer temp = new StringBuffer();
+
+		int curCount = 1;
+		char curChar = in.charAt(0);
+		for(int i =1; i < in.length(); i++) {
+			if(in.charAt(i) == curChar) {
+				curCount++;
+			}
+			else {
+				temp.append(curChar);
+				temp.append(curCount);
+				curChar = in.charAt(i);
+				curCount = 1;
+			}
+		}
+		
+		//end of string
+		temp.append(curChar);
+		temp.append(curCount);
+
+		String retval = temp.toString();
+		if(retval.length() >= in.length()) {
+			return in;
+		}
+
+		return retval;
+	}
+	
+	static public void rotMatrix(int[][] img, int n) {
+
+		int layers = n/2;
+
+		//move down rows
+		for(int i = 0 ; i<layers; i++) {
+			//top left of layer I will be at row and column 0+i
+			//top right will be at row0+i and col n-1-i
+			//bottom right will be at row and column n-1-i
+			//bottom left will be at row n-1-i and column 0+i
+			//90 deg rotation (clockwise) we move topleft->topright->bottomright-> bottomleft
+			//so lets do corners first and go on from there
+
+			//store top right in tmp and set it to top left:
+			int temp =  img[i][n-1-i];
+			img[i][n-1-1] = img[i][i];
+			//store temp in moved pos (top left)
+			img[i][i] = temp;
+			//store  bottom right in temp
+			temp = img[n-1-i][n-1-i];
+			img[n-1-i][n-1-i] = img[i][i];		
+			//put bottom left in top left
+			img[i][i] = img[n-1-i][i];
+			//set botom left from temp
+			img[n-1-i][i] = temp;
+
+			
+
+
+
+
+			//now, for layer I we have row start = I, and col start = I
+			//row end  n-1-i, col end = n-1-i
+			for(int j=i; j< n-1-i; j++) {//move along cols initally			
+				//store target index in temp – 1st target will be row j and column n-1-i
+				temp = img[j][n-1-j];
+				img[j][n-1-j] = img[i][j];
+				//store next val in previous val
+				img[i][j] = img[n-1-i][n-1-j];
+				//set that to temp
+				img[n-1-i][n-1-j] = temp;
+				temp = img [n-1-j][j];
+				img[n-1-j][j] = img[i][j];
+				img[i][j] = temp;
+			}
+		}
+	}
+	
+	static public void printMatrix(int[][] mat, int n) {
+		
+		StringBuffer line = new StringBuffer();
+		
+		for(int i=0; i< n; i++) {
+			line.append(" ");
+			for(int j=0;j<n;j++) {
+				line.append(mat[i][j]);
+				line.append(" ");				
+			}
+			System.out.println(line.toString());
+			line.delete(0,line.length()-1);
+		}
+	}
+				
+				
+				
 }
--- src/testCci.java
+++ src/testCci.java
@@ -27,4 +27,142 @@ public class testCci extends TestCase {
     	
     }
     
+    public final void testspaceReplace() {
+    	String in = "Mr John Smith    ";
+    	int len = 13;
+    	String expected = "Mr%20John%20Smith";
+    	
+    	char[] input = in.toCharArray();
+    	
+    	Cci.spaceReplace(input,len);
+    	
+    	String result = new String(input);
+    	
+    	//System.out.println("Expected: " + result);
+    	//System.out.println("Actual:   " + new String(input));
+    	
+    	assertEquals(expected,result);
+    	
+    }
+    
+    public final void testcompressString() {
+    	String in = new String("aabcccccaaa");
+    	String expected = new String("a2b1c5a3");
+    	
+    	String actual = Cci.compressString(in);
+    	
+    	assertEquals(expected,actual);
+    	
+    	in = "abcde";
+    	expected = "abcde";
+    	
+    	actual = Cci.compressString(in);
+    	
+    	assertEquals(expected,actual);
+    	
+    }
+    
+    public final void testrotMatrix() {
+    	int[][] test1 = new int[3][3];
+    	int[][] exp1 = new int[3][3];
+    	
+    	//test1
+    	//1 2 3
+    	//4 5 6
+    	//7 8 9
+    	
+    	//exp1
+    	//7 4 1
+    	//8 5 2
+    	//9 6 3
+    	
+    	test1[0][0] = 1;
+    	test1[0][1] = 2;
+    	test1[0][2] = 3;
+    	test1[1][0] = 4;
+    	test1[1][1] = 5;
+    	test1[1][2] = 6;
+    	test1[2][0] = 7;
+    	test1[2][1] = 8;
+    	test1[2][2] = 9;
+    	
+    	exp1[0][0] = 7;
+    	exp1[0][1] = 4;
+    	exp1[0][2] = 1;
+    	exp1[1][0] = 8;
+    	exp1[1][1] = 5;
+    	exp1[1][2] = 2;
+    	exp1[2][0] = 9;
+    	exp1[2][1] = 6;
+    	exp1[2][2] = 3;
+    	
+    	int[][] test2 = new int[4][4];
+    	int[][] exp2 = new int[4][4];
+
+    	//test2
+    	// 1, 2, 3, 4
+    	// 5, 6, 7, 8
+    	// 9,10,11,12
+    	//13,14,15,16
+    	
+    	//exp2
+    	//13, 9, 5, 1
+    	//14,10, 6, 2
+    	//15,11, 7, 3
+    	//16,12, 8, 4    	
+    	
+    	test2[0][0] = 1;
+    	test2[0][1] = 2;
+    	test2[0][2] = 3;
+    	test2[0][3] = 4;
+    	test2[1][0] = 5;
+    	test2[1][1] = 6;
+    	test2[1][2] = 7;
+    	test2[1][3] = 8;
+    	test2[2][0] = 9;
+    	test2[2][1] = 10;
+    	test2[2][2] = 11;
+    	test2[2][3] = 12;
+    	test2[3][0] = 13;
+    	test2[3][1] = 14;
+    	test2[3][2] = 15;
+    	test2[3][3] = 16;
+    	
+    	exp2[0][0] = 13;
+    	exp2[0][1] = 9;
+    	exp2[0][2] = 5;
+    	exp2[0][3] = 1;
+    	exp2[1][0] = 14;
+    	exp2[1][1] = 10;
+    	exp2[1][2] = 6;
+    	exp2[1][3] = 2;
+    	exp2[2][0] = 15;
+    	exp2[2][1] = 11;
+    	exp2[2][2] = 7;
+    	exp2[2][3] = 3;
+    	exp2[3][0] = 16;
+    	exp2[3][1] = 12;
+    	exp2[3][2] = 8;
+    	exp2[3][3] = 4;
+    	
+    	Cci.rotMatrix(test1,3);
+    	
+    	Cci.printMatrix(exp1,3);
+    	Cci.printMatrix(test1,3);
+    	
+    	assertEquals(exp1,test1);
+    	
+    	Cci.rotMatrix(test2,4);
+    	
+    	Cci.printMatrix(exp2,4);
+    	Cci.printMatrix(test2,4);    	
+    	
+    	assertEquals(exp2,test2); 	
+    	
+    	
+    	
+    	
+    	
+    }
+    
 }
