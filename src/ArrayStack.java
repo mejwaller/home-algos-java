bdde90c84ae8db0c9e5220ca59942e0901de1294
martin <martin@debian.polytope.org>
2015-04-28 21:52:28 +0100

    Stack impls

---
+++ src/ArrayStack.java
@@ -0,0 +1,35 @@
+//stack implementation using an ArrayList
+import java.util.ArrayList;
+import java.lang.IndexOutOfBoundsException;
+
+public class ArrayStack<T> {
+	
+	private ArrayList<T> array;
+	public int top;
+
+	public ArrayStack() { 
+		top=0;
+		array = new ArrayList<T>();
+	}
+
+	public boolean isEmpty() {
+		if(top==0) return true;
+		else return false;
+	}
+	
+	public void push(T elem) {
+		top++;
+		array.add(top,elem);
+	}
+	
+	public T pop() throws IndexOutOfBoundsException {
+		
+		if(isEmpty()) {
+			throw new IndexOutOfBoundsException("Underflow - stack is empty");
+		}
+		else {
+			top--;
+			return array.remove(top+1);
+		}
+	}
+}
---
+++ src/LlStack.java
@@ -0,0 +1,1 @@
+//Stack implementation using a singly-linked list
