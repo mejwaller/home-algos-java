d9d21c2a38f17b39f7afca4667013af8d3cdd407
martin <martin@debian.polytope.org>
2015-05-03 12:56:07 +0100

    Genericise slinkedlist

--- src/SlinkedList.java
+++ src/SlinkedList.java
@@ -136,42 +136,33 @@ class SlinkedList<T extends Comparable<T>> {
 
 class SNode<T extends Comparable<T>>{
     
-    /* (non-Javadoc)
-     * @see java.lang.Object#hashCode()
-     */
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((data == null) ? 0 : data.hashCode());
-        result = prime * result + ((next == null) ? 0 : next.hashCode());
-        return result;
-    }
-    /* (non-Javadoc)
-     * @see java.lang.Object#equals(java.lang.Object)
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        SNode<T> other = (SNode<T>) obj;
-        if (data == null) {
-            if (other.data != null)
-                return false;
-        } else if (!data.equals(other.data))
-            return false;
-        if (next == null) {
-            if (other.next != null)
-                return false;
-        } else if (!next.equals(other.next))
-            return false;
-        return true;
-    }
-    public SNode(T data) { this.data = data;}
+    
+	
+    
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((data == null) ? 0 : data.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SNode other = (SNode) obj;
+		if (data == null) {
+			if (other.data != null)
+				return false;
+		} else if (!data.equals(other.data))
+			return false;
+		return true;
+	}
+	public SNode(T data) { this.data = data;}
     
     T data;
     SNode<T> next=null;
